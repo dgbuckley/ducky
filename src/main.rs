24e82de1f59f85145487daa7a87fa72b6738b601
Derek Buckley <dgbuckley@protonmail.com>
2023-04-25 18:30:03 -0400

    make a call to chatgpt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+
+use chatgpt::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Getting the API key here
+    let key = args().nth(1).unwrap();
+
+    // Creating a new ChatGPT client.
+    // Note that it requires an API key, and uses
+    // tokens from your OpenAI API account balance.
+    let client = ChatGPT::new(key)?;
+
+    // Sending a message and getting the completion
+    let response = client
+        .send_message("Write a hello world program in rust")
+        .await?;
+
+    println!("Response: {}", response.message().content);
+
+    Ok(())
 }
