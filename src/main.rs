a49ea2183cf0a30f5aa581d466e231681d89e115
Derek Buckley <dgbuckley@protonmail.com>
2023-04-25 21:48:34 -0400

    use the arguments as the prompt to chatgpt

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ use chatgpt::prelude::*;
 #[tokio::main]
 async fn main() -> Result<()> {
     // Getting the API key here
-    let key = args().nth(1).unwrap();
+    let mut arg_it = args();
+    let key = arg_it.nth(1).unwrap();
+    let prompt = arg_it.collect::<Vec<String>>().join(" ");
 
     // Creating a new ChatGPT client.
     // Note that it requires an API key, and uses
     // tokens from your OpenAI API account balance.
     let client = ChatGPT::new(key)?;
 
-    // Sending a message and getting the completion
+    // // Sending a message and getting the completion
     let response = client
-        .send_message("Write a hello world program in rust")
+        .send_message(prompt)
         .await?;
 
     println!("Response: {}", response.message().content);
